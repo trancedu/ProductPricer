dae0c1712e205d538aae4736a34d241a66f48795
trance <trancedu1@gmail.com>
2025-02-14 15:20:41 -0600

    Add typeName to Data.h

--- Data.h
+++ Data.h
@@ -3,24 +3,29 @@
 class Data {
 public:
     virtual ~Data() = default;
+    std::string typeName;
 };
 
 class StockData : public Data {
 public:
     double stock_specific_value = 100.0;
+    StockData() { typeName = "StockData"; }
 };
 
 class BondData : public Data {
 public:
     double bond_specific_value = 200.0;
+    BondData() { typeName = "BondData"; }
 };
 
 class CallableBondData : public BondData {
 public:
     double callable_premium = 5.0;
+    CallableBondData() { typeName = "CallableBondData"; }
 };
 
 class ConvertibleBondData : public BondData {
 public:
     double conversion_ratio = 1.5;
+    ConvertibleBondData() { typeName = "ConvertibleBondData"; }
 }; 
